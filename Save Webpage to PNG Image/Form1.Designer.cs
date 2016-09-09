5c698564736f7b83bd3feefb7b40cbc5d04a21ad
vladislav.tsvek <vladislav.tsvek@teamdev.com>
2016-09-09 09:52:36 +0300

    Browser with the ability to save image in PNG format.

--- Save Webpage to PNG Image/Form1.Designer.cs
+++ Save Webpage to PNG Image/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripAddress = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripUpload = new System.Windows.Forms.ToolStripButton();
+            this.browserView = new DotNetBrowser.WinForms.WinFormsBrowserView();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripAddress,
+            this.toolStripUpload});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(862, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(77, 22);
+            this.toolStripLabel1.Text = "Enter address";
+            // 
+            // toolStripAddress
+            // 
+            this.toolStripAddress.Name = "toolStripAddress";
+            this.toolStripAddress.Size = new System.Drawing.Size(500, 25);
+            this.toolStripAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripAddress_KeyDown);
+            // 
+            // toolStripUpload
+            // 
+            this.toolStripUpload.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripUpload.Image = ((System.Drawing.Image)(resources.GetObject("toolStripUpload.Image")));
+            this.toolStripUpload.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripUpload.Name = "toolStripUpload";
+            this.toolStripUpload.Size = new System.Drawing.Size(73, 22);
+            this.toolStripUpload.Text = "Load image";
+            this.toolStripUpload.ToolTipText = "Load";
+            this.toolStripUpload.Click += new System.EventHandler(this.toolStripUpload_Click);
+            // 
+            // browserView
+            // 
+            this.browserView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserView.Location = new System.Drawing.Point(0, 25);
+            this.browserView.Name = "browserView";
+            this.browserView.Size = new System.Drawing.Size(862, 372);
+            this.browserView.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 397);
+            this.Controls.Add(this.browserView);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripAddress;
+        private System.Windows.Forms.ToolStripButton toolStripUpload;
+        private DotNetBrowser.WinForms.WinFormsBrowserView browserView;
     }
 }
 
